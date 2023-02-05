718686731c2d1598bde0c7f5072fdcb17bc5d05f
Ved K <vedkorla300@gmail.com>
2023-02-05 12:48:16 +0530

    Studied Vectors nesting

--- STL/vectornest.cpp
+++ STL/vectornest.cpp
@@ -1,18 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-void PrintVector(vector<pair<int,int>> &v)
+void PrintVector(vector<int> &v)
 {
-    for(int i = 0; i < v.size(); i++)
+    static int x = 1;
+    cout << "For Vector  " << x << endl;
+    for (int i = 0; i < v.size(); i++)
     {
-        cout << "First element is " << v[i].first << " Second element is " << v[i].second << endl;
+
+        cout << v[i] << " ";
     }
+    cout << endl;
+    cout << "------------------------" << endl;
+    x++;
 }
 
 int main()
 {
-    vector<pair<int, int>> v = {{1, 2}, {2, 3}, {4, 5}};
-    PrintVector(v);
+
+    int N;
+    cin >> N;
+    vector<vector<int>> v; // Ten vectors with zero size!
+    for (int i = 0; i < N; i++)
+    {
+        int n;
+        cin >> n;
+        vector<int> temp;
+        for (int j = 0; j < n; j++)
+        {
+            int x;
+            cin >> x;
+            temp.push_back(x);
+        }
+        v.push_back(temp);
+    }
+    /*
+        For a vector of vectors, create a new temp vector, push back the temp vector in vector of vectors other wise it leads to segmentation fault
+    */
+    for (int i = 0; i < N; i++)
+    {
+        PrintVector(v[i]);
+    }
+    
     return 0;
 }
+
+/*
+    Vector of pairs is a very common topic in Competitive programming.
+*/
