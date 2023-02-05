9b87ed5038355b8a16552b498d4d109a583cb020
Ved K <vedkorla300@gmail.com>
2023-02-05 13:23:06 +0530

    Iterators added

--- STL/iterators.cpp
+++ STL/iterators.cpp
@@ -29,5 +29,34 @@ int main()
     vector<int> v = {2,3,5,6,7};
     vector<int> :: iterator it = v.begin();
     cout << *it << endl;
+    while (it != v.end())
+    {
+        cout << *it << " ";
+        it++;
+    }
+    // Always de-reference iterators to print value stored at that location
+    /*
+        In Maps or sets iterators are not continous like Vectors, thus use it++
+        
+        
+        Very important
+        
+        it++ moves the iterator to the next iterator
+        it = it + 1 moves the iterator to the next memory address!
+    
+    */
+    cout << endl;
+    vector<pair<int,int>> v_p = {{1,2},{2,3},{3,4}};
+    vector<pair<int,int>> :: iterator g = v_p.begin();
+    while (g != v_p.end())
+    {
+        cout << g->first << " " << g->second << endl;
+        g++;
+    }
+
+    /*
+        We can replace (*it).first and (*it).second with it->first and it->second
+    */
+    
     return 0;
 }
